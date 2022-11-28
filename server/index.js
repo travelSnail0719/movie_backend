45fd1768270a2bb77108eeeb65d83809f1d9bc53
travelSnail0719 <ycbkr0719@gmail.com>
2022-11-29 00:17:57 +0900

    mysql 연동

--- server/index.js
+++ server/index.js
@@ -1,10 +1,21 @@
 const express = require('express');
+const mysql = require('mysql');
 const app = express();
 const port = 8000;
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
+
+const config = require('./key');
+
+const db = mysql.createConnection({
+    host : config.host,
+    user : config.user,
+    password : config.password,
+    database : config.database,
+    insecureAuth : config.insecureAuth
+});
 
 app.get('/', (req, res) => res.send('Hello World NodeJs'));
 
