24ac1e2b6b83ebb7dce833bb03cc7ad1ec5a1c51
travelSnail0719 <ycbkr0719@gmail.com>
2022-12-04 23:20:44 +0900

    네이버 API 연동

--- server/index.js
+++ server/index.js
@@ -19,4 +19,35 @@ const db = mysql.createConnection({
 
 app.get('/', (req, res) => res.send('Hello World NodeJs'));
 
+// 영화 검색 요청
+app.get('/search/movies', (req, res) => {
+    const clientId = config.clientId;
+    const clientSecret = config.clientSecret;
+    let apiUrl = `https://openapi.naver.com/v1/search/movie?query=${encodeURI(req.query.query)}`;
+    let params = req.query;
+    for(const [key, value] of Object.entries(params)){
+        if(key != 'query'){
+            apiUrl += `&${key}=${value}`
+        }
+    }
+    console.log('apiUrl', apiUrl);
+    console.log('query', req.query);
+    const request = require('request');
+    const options = {
+        url: apiUrl,
+        headers: {'X-Naver-Client-Id':clientId, 'X-Naver-Client-Secret': clientSecret}
+    };
+    request.get(options, (error, response, body) => {
+        if (!error && response.statusCode == 200) {
+            res.writeHead(200, {'Content-Type': 'text/json;charset=utf-8'});
+            res.end(body);
+        } else {
+            res.status(response.statusCode).end();
+            console.log('error = ' + response.statusCode);
+        }
+    });
+});
+
+
+
 app.listen(port, () => console.log(`let's start listening on port ${port}`));
