690c353860479eabeab9e3c55f50d5b98776ad19
travelSnail0719 <ycbkr0719@gmail.com>
2022-12-05 23:50:49 +0900

    cors추가

--- server/index.js
+++ server/index.js
@@ -3,10 +3,11 @@ const mysql = require('mysql');
 const app = express();
 const port = 8000;
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
-
+app.use(cors());
 const config = require('./key');
 
 const db = mysql.createConnection({
