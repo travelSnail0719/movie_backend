b43f711487f5400611b0854b3c1ce25d7169d012
travelSnail0719 <ycbkr0719@gmail.com>
2022-12-12 23:13:20 +0900

    회원가입 로직 추가

--- server/auth/register.js
+++ server/auth/register.js
@@ -18,17 +18,28 @@ const register = (req, res) => {
     const userGen = req.body.userGen;
     const userBir = req.body.userBir;
     const userMail = req.body.userMail;
-    
-    const sqlQuery = 'INSERT INTO T_USER(USER_ID, USER_NM, USER_PW, USER_GEN, USER_BIR, USER_MAIL) VALUES (?, ?, ?, ?, ?, ?);';
-    db.query(sqlQuery, [userId, userNm, userPw, userGen, userBir, userMail], (err, result) => {
-        if(err){
-            res.send(err);
-            console.log('err', err);
-        } else {
-            res.send(result);
-            console.log('result', result);
-        }
-    }) 
-}
 
+    // 모든 정보 다 들어왔는지 채크
+    if(userId != null && userNm != null && userPw != null && userGen != null && userBir != null && userMail != null){
+        // 아이디 중복체크
+        db.query('SELECT USER_ID FROM T_USER WHERE USER_ID = ?', [userId], (error, checkDuplicate) => {
+            // 아이디 중복이 없을 때
+            if(checkDuplicate.length <= 0){
+                const sqlQuery = 'INSERT INTO T_USER(USER_ID, USER_NM, USER_PW, USER_GEN, USER_BIR, USER_MAIL) VALUES (?, ?, ?, ?, ?, ?);';
+                db.query(sqlQuery, [userId, userNm, userPw, userGen, userBir, userMail], (err, result) => {
+                    if(err){
+                        res.send(err);
+                        console.log('err', err);
+                    } else {
+                        res.send(result);
+                        console.log('result', result);
+                    }
+                }) 
+            } else {
+                res.json({message : '동일한 아이디가 존재합니다.'});
+                console.log('error', error);
+            }
+        })
+    }
+}
 module.exports = {register};
